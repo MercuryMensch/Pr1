6b4745d57726bd8827a7fe62dfa7cf933a8f6cb3
MercuryMensch <66737730+MercuryMensch@users.noreply.github.com>
2022-03-12 09:02:51 +0300

    Add files via upload

---
+++ Practice1/Practice1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    class lib
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+        public int year { get; set; }
+        public void Print()
+        {
+            Console.WriteLine($"имя: {name}  адрес: {address} год основания: {year}");
+        }
+        static void Main(string[] args)
+        { 
+            lib Nan = new lib();
+            Nan.name = "Billy";
+            Nan.address = "Hruzkogo street 24";
+            Nan.year = 1;
+            Nan.Print();
+            Console.ReadKey();
+
+        }
+    }
+}
